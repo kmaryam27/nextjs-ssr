30c907eb2708d03c45add684f396692b547e90d3
k_maryam27 <k_maryam27@yahoo.com>
2020-04-18 00:25:30 -0400

    next Link

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,14 @@
+import Link from 'next/link'
 export default () => {
     return(
     <div>
+        {/* this link no refresh page and make a new html page and will use history */}
+        <Link href="/">
+            <a>go home page with next/link</a>
+        </Link>
+        <br/>
+        {/* refresh page */}
+        <a href="/">go home page with simple anchot tag</a>
         <h2>About</h2>
     </div>
     )
